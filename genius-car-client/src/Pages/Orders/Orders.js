12d8e1dbe4f8e9a2685435a01d48b3d59f840dd4
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-11-02 14:41:03 +0000

    Use Query parameter to load user specific data

--- genius-car-client/src/Pages/Orders/Orders.js
+++ genius-car-client/src/Pages/Orders/Orders.js
@@ -1,11 +1,26 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const Orders = () => {
   const { user } = useContext(AuthContext);
-  return <div>
-    
-  </div>;
+
+  const [orders, setOrders] = useState({});
+
+  // const url = `http://localhost:5000/orders?email=${user.email}`;
+
+  useEffect(() => {
+    // fetch(url)
+    fetch(`http://localhost:5000/orders?email=${user?.email}`)
+      .then((response) => response.json())
+      .then((data) => setOrders(data))
+      .catch((error) => console.log(error));
+  }, [user?.email]);
+
+  return (
+    <div>
+      <h2 className="text-4xl">You have {orders.length} orders</h2>
+    </div>
+  );
 };
 
 export default Orders;
