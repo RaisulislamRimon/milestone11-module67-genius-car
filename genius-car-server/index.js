9c8f917b4e70fcac40ab2be2d7e74dabdc579648
RaisulislamRimon <rimonppip0@gmail.com>
2022-11-02 13:41:43 +0600

    added dot env in express application

--- genius-car-server/index.js
+++ genius-car-server/index.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+console.log(process.env.DB_USER);
 
 app.get("/", (req, res) => {
   res.send("geinus-car-server is running");
