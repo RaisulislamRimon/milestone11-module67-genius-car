2cb0382337df9c7734ce654b2e94071b10807d3b
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-11-02 11:17:58 +0000

    update form

--- genius-car-client/src/Pages/Checkout/Checkout.js
+++ genius-car-client/src/Pages/Checkout/Checkout.js
@@ -4,34 +4,44 @@ import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const Checkout = () => {
   const service = useLoaderData();
-  const { title } = service;
+  const { title, price } = service;
   const { user } = useContext(AuthContext);
   return (
     <div>
-      <div className="grid grid-cols-1 lg:grid-cols-2">
-        <input
-          type="text"
-          placeholder="First Name"
-          className="input input-bordered input-ghost w-full"
-        />
-        <input
-          type="text"
-          placeholder="Last Name"
-          className="input input-bordered input-ghost w-full"
-        />
-        <input
-          type="text"
-          placeholder="Your Phone"
-          className="input input-bordered input-ghost w-full"
-        />
-        <input
-          type="text"
-          placeholder="Your Email"
-          className="input input-bordered input-ghost w-full"
-          defaultValue={user?.email}
-          readOnly
-        />
-      </div>
+      <form>
+        <h2 className="text-4xl text-center">
+          You are about to order: {title}
+        </h2>
+        <h4 className="text-3xl">Price : ${price}</h4>
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="input input-bordered input-ghost w-full"
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="input input-bordered input-ghost w-full"
+          />
+          <input
+            type="text"
+            placeholder="Your Phone"
+            className="input input-bordered input-ghost w-full"
+          />
+          <input
+            type="text"
+            placeholder="Your Email"
+            className="input input-bordered input-ghost w-full"
+            defaultValue={user?.email}
+            readOnly
+          />
+        </div>
+      </form>
+      <textarea
+        className="textarea textarea-bordered h-24 w-full"
+        placeholder="Your Message"
+      ></textarea>
     </div>
   );
 };
