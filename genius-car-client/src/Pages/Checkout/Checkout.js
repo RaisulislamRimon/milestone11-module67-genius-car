58150a1a404e905f4966abe6a32f26a8ff7d2436
MD. Raisul Islam Rimon <51745662+RaisulislamRimon@users.noreply.github.com>
2022-11-02 11:29:18 +0000

    Implement Checkout Form to create order object

--- genius-car-client/src/Pages/Checkout/Checkout.js
+++ genius-car-client/src/Pages/Checkout/Checkout.js
@@ -4,32 +4,59 @@ import { AuthContext } from "../../contexts/AuthProvider/AuthProvider";
 
 const Checkout = () => {
   const service = useLoaderData();
-  const { title, price } = service;
+  const { _id, title, price } = service;
   const { user } = useContext(AuthContext);
+
+  const handlePlaceOrder = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    // const firstName = form.firstName.value;
+    // const lastName = form.lastName.value;
+    const name = `${form.firstName.value} ${form.lastName.value}`;
+    const phone = form.phone.value;
+    // const email = form?.email.value;
+    const email = form?.email || "Unregistered";
+    const message = form?.message.value;
+
+    const order = {
+      service: _id,
+      serviceName: title,
+      price: price,
+      customer: name,
+      email,
+      phone,
+      message,
+    };
+  };
+
   return (
     <div className="my-20">
-      <form>
+      <form onSubmit={handlePlaceOrder}>
         <h2 className="text-4xl text-center">
           You are about to order: {title}
         </h2>
         <h4 className="text-3xl">Price : ${price}</h4>
         <div className="grid grid-cols-1 lg:grid-cols-2">
           <input
+            name="firstName"
             type="text"
             placeholder="First Name"
             className="input input-bordered input-ghost w-full"
           />
           <input
+            name="lastName"
             type="text"
             placeholder="Last Name"
             className="input input-bordered input-ghost w-full"
           />
           <input
+            name="phone"
             type="text"
             placeholder="Your Phone"
             className="input input-bordered input-ghost w-full"
           />
           <input
+            name="email"
             type="text"
             placeholder="Your Email"
             className="input input-bordered input-ghost w-full"
@@ -39,6 +66,7 @@ const Checkout = () => {
         </div>
       </form>
       <textarea
+        name="message"
         className="textarea textarea-bordered h-24 w-full"
         placeholder="Your Message"
       ></textarea>
